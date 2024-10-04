a42e0f933c1d773a9eb2fe61974a45b36410e66c
Gustavo Takeda <gustavo.takeda@eletraenergy.com>
2024-10-04 14:04:33 -0300

    xoxo myself

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+public class SocketServer
+{
+    TcpClient clientSocket;
+    string server;
+    int port;
+
+    public void Connect()
+    {
+        clientSocket.Connect(server, port);
+    }
+
+    public void SendString(string message)
+    {
+        byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+        NetworkStream stream = this.clientSocket.GetStream();
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Sent: {0}", message, port);
+    }
+
+    public void Close()
+    {
+        clientSocket.Close();
+    }
+
+    public string Receive()
+    {
+        NetworkStream stream = this.clientSocket.GetStream();
+        byte[] data = new byte[512];
+        Int32 r_bytes = stream.Read(data, 0, data.Length);
+        string s = System.Text.Encoding.ASCII.GetString(data, 0, r_bytes);
+        return s;
+    }
+
+    public SocketServer(string server, int port)
+    {
+        this.clientSocket = new TcpClient(server, port);
+        this.port = port;
+        this.server = server;
+    }
+}
+
+public class ESPComms
+{
+    public static void Main(string[] args)
+    {
+        SocketServer server = new SocketServer("q", 8888);
+        server.Connect();
+        string? message = "";
+        while(true)
+        {
+            message = Console.ReadLine();
+            server.SendString(message);
+            Console.Write(server.Receive());
+        }
+    }
+}
